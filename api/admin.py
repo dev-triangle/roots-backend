89552de4ceb8cc3a1b09af417a49cce1d0cab2a2
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-12-24 22:23:22 +0530

    placed space

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import (Trending,Place,Activities,Guide,Festival,Item)
+from .models import (Trending , Place , Activities , Guide , Festival , Item)
 # Register your models here.
-
 admin.site.register(Trending)
 admin.site.register(Place)
 admin.site.register(Activities)
