bf4713612138462844f0a4ab2584efa930b37fbc
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-01-08 09:44:56 +0530

    completed signup page

--- rootsbackend/settings.py
+++ rootsbackend/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 REST_FRAMEWORK={
     'DEFAULT_PERMISSION_CLASSES':('rest_framework.permissions.AllowAny',),
